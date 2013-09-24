a1e13af105c01f7934cbf10fa2a148f11b412db1
Ivan Acosta-Rubio <ivan@bakedweb.net>
2013-09-24 12:48:56 -0500

    Training a Category and getting the probability out of it.

--- hbayes.hs
+++ hbayes.hs
@@ -1,2 +1,15 @@
-sProb :: String -> [String] -> Float
-sProb x xs = fromIntegral(length(filter(==x) xs)) / fromIntegral (length xs)
+-- TODO: Being able to read categories from a file
+data Category = Vegetables | Proteins
+    deriving (Show)
+
+data TrainedCategory = TrainedCategory Category [String]
+    deriving (Show)
+
+dataFromTrainedCategory :: TrainedCategory -> [String]
+dataFromTrainedCategory (TrainedCategory cat xs) = xs
+
+sProb :: (String, [String]) -> Float
+sProb (x, xs)= fromIntegral(length(filter(==x) xs)) / fromIntegral (length xs)
+
+categoryProb :: (String, TrainedCategory) -> Float
+categoryProb (x, t) = sProb(x, dataFromTrainedCategory t)
