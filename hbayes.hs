214bc863f758d004595fb66f52c4bd6831fa5631
Ivan Acosta-Rubio <ivan@bakedweb.net>
2013-09-24 20:11:01 -0500

    not needed.

--- hbayes.hs
+++ hbayes.hs
@@ -14,14 +14,6 @@ categoryFromTRainedCategory (TrainedCategory c xs) = c
 sProb :: (String, [String]) -> Float
 sProb (x, xs)= fromIntegral(length(filter(==x) xs)) / fromIntegral (length xs)
 
-categoryProb :: (String, TrainedCategory) -> (Category, Float)
-categoryProb (x, t) = (categoryFromTRainedCategory(t), sProb(x, dataFromTrainedCategory t))
-
-probDist :: (String, [TrainedCategory]) -> [ (Category, Float) ]
-probDist(x, y:ys)  =  categoryProb(x, y)  : probDist(x, ys)
-probDist(x,[]) = []
-
-
 totalTrainingSet :: [TrainedCategory] -> Int
 totalTrainingSet (x:xs) = length(dataFromTrainedCategory(x)) + totalTrainingSet(xs)
 totalTrainingSet [] = 0
